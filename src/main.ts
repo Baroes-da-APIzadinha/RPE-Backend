99ff6068e119612d4db3bd2c4abe327cf204ad7e
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2025-06-24 18:40:38 -0300

    fix: concertando o erro de cors, para habilitar requisições pelo front

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true, // use isso se seu front usa cookies/autenticação
+  });
   await app.listen(process.env.PORT ?? 3000);
+
+
+
 }
 bootstrap();
