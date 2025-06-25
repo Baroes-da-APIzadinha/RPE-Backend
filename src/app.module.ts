daca81acd5f5837cb9fcfaa5c6cc2d956d737a0c
joaofellows <joaovictorfellowsrabelo@gmail.com>
2025-06-25 18:29:40 -0300

    feat(avaliacoesmodule): criaçao do modulo de avaliaçoes

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from './database/prismaService';
 import { AssociacaoCriterioCicloModule } from './criterioCiclo/criterioCiclo.module';
 import { CicloModule } from './ciclo/ciclo.module';
+import { AvaliacoesController } from './avaliacoes/avaliacoes.controller';
+import { AvaliacoesService } from './avaliacoes/avaliacoes.service';
+import { AvaliacoesModule } from './avaliacoes/avaliacoes.module';
 
 
 @Module({
-  imports: [ColaboradorModule, CriteriosModule, CicloModule, AssociacaoCriterioCicloModule, AuthModule, JwtModule.register({})],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [ColaboradorModule, CriteriosModule, CicloModule, AssociacaoCriterioCicloModule, AuthModule, JwtModule.register({}), AvaliacoesModule],
+  controllers: [AppController, AvaliacoesController],
+  providers: [AppService, PrismaService, AvaliacoesService],
 })
 export class AppModule {}
