50997b0456a3608293d231270b638b60e20fc4ba
joaofellows <joaovictorfellowsrabelo@gmail.com>
2025-07-04 01:43:18 -0300

    fix(delete-comment): deletando comentários para limpeza e clareza do código

--- src/avaliacoes/avaliacoes.service.ts
+++ src/avaliacoes/avaliacoes.service.ts
@@ -1181,17 +1181,3 @@ export class AvaliacoesService {
         return avaliacoes;
     }
 }
-
-/*interface RelatorioItem {
-    idColaborador: string;
-    nomeColaborador: string;
-    cargoColaborador: string;
-    trilhaColaborador: string | null;
-    equipeColaborador: string | null;
-    notas: {
-        notaAuto: number | null;
-        nota360media: number | null;
-        notaGestor: number | null;
-        discrepancia: number | null | undefined;
-    };
-} */
