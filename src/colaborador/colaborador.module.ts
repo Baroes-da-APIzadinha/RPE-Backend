037ed490f35049e34bb4d76570bdacd2d8f90764
Gabriel Calado <gcalado501@gmail.com>
2025-07-07 20:16:14 -0300

    fix(colaborador): gET/colaborador/pilar/historico/{idColaborador}
    Esse endpoint estava retornando as notas por criterio e nao por pilar

--- src/colaborador/colaborador.module.ts
+++ src/colaborador/colaborador.module.ts
@@ -4,9 +4,10 @@ import { ColaboradorController } from './colaborador.controller';
 import { PrismaService } from 'src/database/prismaService';
 import { AvaliacoesService } from '../avaliacoes/avaliacoes.service';
 import { CicloService } from '../ciclo/ciclo.service';
+import { CriteriosService } from '../criterios/criterios.service';
 
 @Module({
-  providers: [ColaboradorService, PrismaService, AvaliacoesService, CicloService],
+  providers: [ColaboradorService, PrismaService, AvaliacoesService, CicloService, CriteriosService],
   controllers: [ColaboradorController],
   exports: [ColaboradorService],
 })
