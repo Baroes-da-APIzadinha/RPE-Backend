29655fdb07c67c24fe846210974cec8129f2b383
Marcelo Melo <mpedrosa.melo@gmail.com>
2025-07-08 18:17:48 -0300

    fix(ia.module): adicionando AvaliacoesService no ia.module

--- src/IA/ia.module.ts
+++ src/IA/ia.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { IaController } from './ia.controller';
 import { IaService } from './ia.service';
 import { PrismaService } from '../database/prismaService';
-
+import { AvaliacoesModule } from '../avaliacoes/avaliacoes.module'; // ajuste o caminho se necessário
+import { AvaliacoesService } from 'src/avaliacoes/avaliacoes.service';
 @Module({
     controllers: [IaController],
-    providers: [IaService, PrismaService],
+    providers: [IaService, PrismaService, AvaliacoesService],
+    imports: [AvaliacoesModule], // <-- Adicione aqui
+    exports: [IaService],
 })
 export class IaModule {}
