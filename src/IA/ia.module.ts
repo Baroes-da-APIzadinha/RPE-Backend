38fb4725ae8b6306c5fd2c85381962247097de8e
Marcelo Melo <mpedrosa.melo@gmail.com>
2025-07-13 00:01:58 -0300

    fix(auditoria/hash): ajustando erros de imports em auditoria e hash service

--- src/IA/ia.module.ts
+++ src/IA/ia.module.ts
@@ -4,9 +4,11 @@ import { IaService } from './ia.service';
 import { PrismaService } from '../database/prismaService';
 import { AvaliacoesModule } from '../avaliacoes/avaliacoes.module'; 
 import { AvaliacoesService } from 'src/avaliacoes/avaliacoes.service';
+import { HashService } from 'src/common/hash.service';
+
 @Module({
     controllers: [IaController],
-    providers: [IaService, PrismaService, AvaliacoesService],
+    providers: [IaService, PrismaService, AvaliacoesService, HashService],
     imports: [AvaliacoesModule], 
     exports: [IaService],
 })
--- src/colaborador/colaborador.module.ts
+++ src/colaborador/colaborador.module.ts
@@ -6,9 +6,11 @@ import { AvaliacoesService } from '../avaliacoes/avaliacoes.service';
 import { CicloService } from '../ciclo/ciclo.service';
 import { CriteriosService } from '../criterios/criterios.service';
 import { EqualizacaoService } from 'src/equalizacao/equalizacao.service';
+import { HashService } from 'src/common/hash.service';
+import { AuditoriaService } from 'src/auditoria/auditoria.service';
 
 @Module({
-  providers: [ColaboradorService, PrismaService, AvaliacoesService, CicloService, CriteriosService, EqualizacaoService],
+  providers: [ColaboradorService, PrismaService, AvaliacoesService, CicloService, CriteriosService, EqualizacaoService, HashService, AuditoriaService],
   controllers: [ColaboradorController],
   exports: [ColaboradorService],
 })
