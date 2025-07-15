637c196b04cfd0e164c66832892a5c7420513690
joaofellows <joaovictorfellowsrabelo@gmail.com>
2025-07-15 15:41:28 -0300

    docs(dtos): adicionando ApiProperty nos DTOs para documentaçao mais detalhada

--- src/equalizacao/equalizacao.dto.ts
+++ src/equalizacao/equalizacao.dto.ts
@@ -1,21 +1,26 @@
 import { IsNotEmpty, IsUUID, IsInt, IsString, Min, Max, IsOptional, IsEnum, IsDecimal } from 'class-validator';
 import { preenchimentoStatus } from '@prisma/client';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateEqualizacaoDto {
+  @ApiProperty({ description: 'ID do ciclo'})
   @IsUUID()
   @IsNotEmpty()
   idCiclo: string;
 }
 
 export class UpdateEqualizacaoDto {
+  @ApiProperty({ description: 'Nota da equalização' })
   @IsDecimal()
   @IsNotEmpty()
   notaAjustada: number;
 
+  @ApiProperty({ description: 'justificativa para a nota' })
   @IsString()
   @IsNotEmpty()
   justificativa: string;
 
+  @ApiPropertyOptional({ description: 'status de preenchimento', enum: preenchimentoStatus })
   @IsEnum(preenchimentoStatus)
   @IsOptional()
   status?: preenchimentoStatus;
--- src/projetos/alocacao.dto.ts
+++ src/projetos/alocacao.dto.ts
@@ -1,24 +1,30 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsDateString, IsUUID } from 'class-validator';
 
 export class CreateAlocacaoDto {
+  @ApiProperty({ description: 'ID do colaborador' })
   @IsUUID()
   @IsNotEmpty({ message: 'O ID do colaborador é obrigatório.' })
   idColaborador: string;
 
+  @ApiProperty({ description: 'Data de entrada do colaborador' })
   @IsDateString({}, { message: 'A data de entrada deve ser uma data válida.' })
   @IsNotEmpty({ message: 'A data de entrada é obrigatória.' })
   dataEntrada: string;
 
+  @ApiPropertyOptional({ description: 'Data de saída do colaborador' })
   @IsDateString({}, { message: 'A data de saída deve ser uma data válida.' })
   @IsOptional()
   dataSaida?: string;
 }
 
 export class UpdateAlocacaoDto {
+  @ApiPropertyOptional({ description: 'Data de entrada do colaborador' })
   @IsDateString({}, { message: 'A data de entrada deve ser uma data válida.' })
   @IsOptional()
   dataEntrada?: string;
 
+  @ApiPropertyOptional({ description: 'Data de saída do colaborador' })
   @IsDateString({}, { message: 'A data de saída deve ser uma data válida.' })
   @IsOptional()
   dataSaida?: string;
